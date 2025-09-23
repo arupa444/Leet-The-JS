3aab48b52880ae94c68fac00d72fce98f849cbdd
arupanandaswain <arupaswain7735@gmail.com>
2025-09-23 16:02:55 +0530

    update on js

--- 23Sep.js
+++ 23Sep.js
@@ -7,17 +7,18 @@ var compareVersion = function(version1, version2) {
     store1 = version2.split(".").map((ele)=>{
         return Number(ele)
     })
-    console.log(store, store1)
 
-    store.forEach((ele, index)=>{
-        if
-    })
 
-    if(version1 < version2){
-        return 1
-    }else{
-        return -1
+    for(let i=0; i<Math.max(store.length, store1.length); i++){
+        ele = (store[i] !== undefined) ? store[i] : 0
+        ele1 = (store1[i] !== undefined) ? store1[i] : 0
+        if ( ele < ele1){
+            return -1
+        }else if(ele > ele1){
+            return 1
+        }
     }
+    return 0
 };
 
-console.log(compareVersion("1.20", "2.21"))
+console.log(compareVersion( "1", "1.1"))
